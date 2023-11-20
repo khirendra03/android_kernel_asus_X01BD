217dc4be3499001d0e6ad904b759f899015a183c
khirendra <khirendrapatel3@gmail.com>
2023-11-20 12:57:19 +0000

    X01BD: update regen.sh
    Signed-off-by: khirendra <khirendrapatel3@gmail.com>

--- regen.sh
+++ regen.sh
@@ -1,1 +1,1 @@
-export ARCH=arm64 && export SUBARCH=arm64 && make X00TD_defconfig && mv .config arch/arm64/configs/X00TD_defconfig && git add arch/arm64/configs/X00TD_defconfig && git commit -m "defconfig: Regen" -s
+export ARCH=arm64 && export SUBARCH=arm64 && make X01BD_defconfig && mv .config arch/arm64/configs/X01BD_defconfig && git add arch/arm64/configs/X01BD_defconfig && git commit -m "defconfig: Regen" -s
